50d56b2ffe35578a9651f525080b694876cf2c28
Richard Wells <rwells1703@gmail.com>
2018-07-26 23:40:48 +0100

    Moved from local files to using flask as a web server. Also set up the basic style and structure of the web page.

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+
+@app.route("/")
+def index():
+	return render_template("index.html", active="index")
+
+@app.route("/about_matrices")
+def about_matrices():
+	return render_template("about_matrices.html", active="about_matrices")
+
+@app.route("/matrix_tools")
+def matrix_tools():
+        return render_template("matrix_tools.html", active="matrix_tools")
+
+@app.route("/matrix_visualiser")
+def matrix_visualiser():
+	return render_template("matrix_visualiser.html", active="matrix_visualiser")
+
+@app.route("/help")
+def help():
+        return render_template("help.html", active="help")
