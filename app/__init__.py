a051cb2f7fefc9aa88364fd3227392c56fef5216
Richard Wells <rwells1703@gmail.com>
2018-08-11 18:33:31 +0100

    Removed loading screen from the about page.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask
+import flask
 
 # Changes the default behaviour of flask so that the template folder and static folder are all pointed towards a single "webroot" directory
 webroot = os.path.dirname(os.path.abspath(__file__))+"\\webroot"
-
-app = Flask(__name__, template_folder = webroot, static_folder = webroot, static_url_path = "")
+	
+app = flask.Flask(__name__, template_folder = webroot, static_folder = webroot, static_url_path = "")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,29 @@
-from flask import render_template
+import flask
 from app import app
-
+	
 @app.route("/")
 def index():
-	return render_template("index.html", active="index")
+	return flask.render_template("index.html", active="index")
 
 @app.route("/about_matrices")
 def about_matrices():
-	return render_template("about_matrices.html", active="about_matrices")
+	return flask.render_template("about_matrices.html", active="about_matrices")
 
 @app.route("/matrix_calculator")
-def matrix_combo():
-	return render_template("matrix_calculator/matrix_calculator.html", active="matrix_calculator")
+def matrix_calculator():
+	return flask.render_template("matrix_calculator/matrix_calculator.html", active="matrix_calculator")
 
 @app.route("/help")
 def help():
-        return render_template("help.html", active="help")
+	return flask.render_template("help.html", active="help")
+
+# This makes sure that JS files are not cached in the browser cache
+# This is useful because it means that I do not have to clear browser cache every time I make a change to a JS script during development
+@app.after_request
+def after_request(response):
+	file_extension = flask.request.__dict__["environ"]["PATH_INFO"].split(".")[-1]
+	if file_extension == "js":
+		response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+		response.headers["Pragma"] = "no-cache"
+		response.headers["Expires"] = "0"
+	return response
